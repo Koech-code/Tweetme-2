450c5143c555b119075956a68d3176745d22de94
Koech-code <nixonkipkorir01@gmail.com>
2022-09-27 01:19:56 +0300

    create views for my models

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,13 @@
 import random
 from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
-
+from .models import UploadVideo, PostImage, CommentImage, CommentVideo
+from .serializers import VideoSerializer, ImageSerializer, CommentImageSerializer, CommentVideoSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -16,3 +20,24 @@ def tweets_list_view(request, *args, **kwargs):
 
 def tweets_detail_view(request, tweet_id, *args, **kwargs):
     return render(request, "tweets/detail.html", context={"tweet_id": tweet_id})
+
+class Videos(APIView):
+    def get(self, request):
+        if request.method == 'GET':
+            
+            uploadedvideos = UploadVideo.objects.all()
+            serializer = VideoSerializer(uploadedvideos, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        
+        serializers = VideoSerializer(data=request.data)
+ 
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,
+            status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializers.errors, 
+            status=status.HTTP_400_BAD_REQUEST)
