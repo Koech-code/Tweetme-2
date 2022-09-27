31caba14a364dd490be4860abc3a01aee724c3a1
Koech-code <nixonkipkorir01@gmail.com>
2022-09-27 17:36:17 +0300

    add global to list of available urls

--- tweets/urls.py
+++ tweets/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns=[
     path('videos/', views.Videos.as_view(), name='videos'),
+    path('images/', views.Images.as_view(), name='images'),
+    path('commentvideo/<int:pk>/', views.CommentOnAVideo.as_view(), name='commentvideo'),
+    path('commentimage/<int:id>/', views.CommentOnAnImage.as_view(), name='commentvideo'),
+    path('global/', views.tweets_list_view),
 ]
 
 if settings.DEBUG:
