9a05fd4914eab2ca5351e92c16c2f34d350be7f9
Koech-code <nixonkipkorir01@gmail.com>
2022-09-27 17:37:15 +0300

    create views to post and get videos and images

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,5 @@
 import random
+from urllib import response
 from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from rest_framework.response import Response
@@ -8,6 +9,7 @@ from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 from .models import UploadVideo, PostImage, CommentImage, CommentVideo
 from .serializers import VideoSerializer, ImageSerializer, CommentImageSerializer, CommentVideoSerializer
+from tweets import serializers
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -41,3 +43,64 @@ class Videos(APIView):
         else:
             return Response(serializers.errors, 
             status=status.HTTP_400_BAD_REQUEST)
+
+class CommentOnAVideo(APIView):
+    def post(self, request, *args, **kwags):
+
+        serializer = CommentVideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, 
+            status=status.HTTP_201_CREATED)            
+        else:
+            return Response(serializer.errors, 
+            status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, pk):
+        if request.method == 'GET':
+            commentedvideos = CommentVideo.objects.filter(uploadedvideo_id=pk)
+            serialize = CommentVideoSerializer(commentedvideos, many=True)
+
+        return Response(serialize.data)
+  
+        
+class Images(APIView):
+    def post(self, request, *args, **kwargs):
+            
+            serializers = ImageSerializer(data=request.data)
+    
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data,
+                status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializers.errors, 
+                status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        if request.method == 'GET':
+            
+            postedimages = PostImage.objects.all()
+            serializer = ImageSerializer(postedimages, many=True)
+
+        return Response(serializer.data)         
+
+
+class CommentOnAnImage(APIView):
+    def post(self, request, *args, **kwags):
+
+        serializer = CommentImageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, 
+            status=status.HTTP_201_CREATED)            
+        else:
+            return Response(serializer.errors, 
+            status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, id):
+        if request.method == 'GET':
+            commentedvideos = CommentImage.objects.filter(postedimage_id=id)
+            serializers = CommentImageSerializer(commentedvideos, many=True)
+
+        return Response(serializers.data)
