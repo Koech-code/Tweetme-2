68c4cee5fe815ce5fee0ea589515609908e43994
Koech-code <nixonkipkorir01@gmail.com>
2022-10-06 08:20:06 +0300

    register all the components here

--- tweetme2-web/src/tweets/create.js
+++ tweetme2-web/src/tweets/create.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {apiTweetCreate} from './lookup'
 import CreateTweet from './createtweets'
 import UploadVideo from './uploadvideo'
+import CommentVideos from './commentvideos'
+
 
 export function TweetCreate(props){
   const textAreaRef = React.createRef()
@@ -25,6 +27,7 @@ export function TweetCreate(props){
     return <div className={props.className}>
       <div><CreateTweet></CreateTweet></div>
       <div><UploadVideo></UploadVideo></div>
+      <div><CommentVideos></CommentVideos></div>
      
           <form onSubmit={handleSubmit}>
             
